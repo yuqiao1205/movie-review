d65deb2e3b9eaad69419bb853ab44e7d8dab824d
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-10 19:06:19 -0700

    update image size to  width={320} height={200} used flex

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -40,12 +40,12 @@ export default function Card({ result }) {
 
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
-      <Link href={`/movie/${result.id}`}>
+      <Link href={`/movie/${result.id}`} className='flex justify-center'>
         <img
           src={imageUrl}
           alt=''
-          width={500}
-          height={300}
+          width={320}
+          height={200}
           className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
         />
       </Link>
