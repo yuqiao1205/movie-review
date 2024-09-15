81ccf0aad9b8f09c79f89bab8e8099747f54e886
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-09-15 14:06:41 -0700

    update next changed apikey

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,20 @@
-import Results from '@/components/Results';
-import fetch from 'node-fetch'; // 添加这一行
+import Results from "@/components/Results";
+import fetch from "node-fetch"; // 添加这一行
 
-const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const API_KEY = process.env.MOVIE_API_KEY;
 
 export default async function Home({ searchParams }) {
-  const genre = searchParams.genre || 'fetchTopRated';
-  const url = `https://api.themoviedb.org/3${
-    genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
-  }?api_key=${MOVIE_API_KEY}&language=en-US&page=1`;
-
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODk2NmMxMzQ0ZDYyMzcyNjdlM2QzM2RmODlhMjU2YiIsInN1YiI6IjY2M2Q1YTFjNzYwMTE1MmZhYjBlYmQ1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y3mbjByyvsWYM8FeHiCi0r55T5YU6H4GiZT7vqgqk9I'
-    }
-  };
-
-  const res = await fetch(url, options);
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
   const data = await res.json();
-
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-
   const results = data.results;
 
   return (
@@ -32,3 +23,34 @@ export default async function Home({ searchParams }) {
     </div>
   );
 }
+
+// export default async function Home({ searchParams }) {
+//   const genre = searchParams.genre || "fetchTopRated";
+//   const url = `https://api.themoviedb.org/3${
+//     genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+//   }?api_key=${MOVIE_API_KEY}&language=en-US&page=1`;
+
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzgzODQ5OWNjMjAxMTdmNjZjNGEyNWM4NDM0OTE3MSIsIm5iZiI6MTcyNjQzMzA3Mi45MDY3MTYsInN1YiI6IjY2ZTBjYTMxZTc3MGVhMWJiOTM2ODQ5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-YlR7i9y5mBYvFeCxmkE-igiBtW5Lv2Fxizs1ow3Ja8",
+//     },
+//   };
+
+//   const res = await fetch(url, options);
+//   const data = await res.json();
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+
+//   const results = data.results;
+
+//   return (
+//     <div>
+//       <Results results={results} />
+//     </div>
+//   );
+// }
--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,34 +1,17 @@
-import Results from '@/components/Results';
-
-const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+import Results from "@/components/Results";
 
 export default async function SearchPage({ params }) {
   const seachTerm = params.searchTerm;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`;
-  // const res = await fetch(
-  //   `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
-  // );
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODk2NmMxMzQ0ZDYyMzcyNjdlM2QzM2RmODlhMjU2YiIsInN1YiI6IjY2M2Q1YTFjNzYwMTE1MmZhYjBlYmQ1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y3mbjByyvsWYM8FeHiCi0r55T5YU6H4GiZT7vqgqk9I'
-    }
-  };
-  const res = await fetch(url, options);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+  );
   const data = await res.json();
-
-  console.log( "data=" + data);
   const results = data.results;
-  console.log("search reslut=" + results);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
   return (
     <div>
       {results &&
-        results.length === 
-        <h1 className='text-center pt-6'>No results found</h1>}
+        results.length ===
+        <h1 className="text-center pt-6">No results found</h1>}
       {results && <Results results={results} />}
     </div>
   );
